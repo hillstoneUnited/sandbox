3416bf039d48c85d969257e50255e34d2d0077f9
ganow <y.nagano.92@gmail.com>
2012-06-07 16:42:24 +0900

    these commits are practices
    practice for git command
    practice 1
    
    this is the test
    
    practice 2
    
    this is the test2
    
    practice 3
    
    this is the test3

--- test_ganow.py
+++ test_ganow.py
@@ -1,3 +1,5 @@
+#coding: UTF-8
+
 print "Hello World!!"
 print "hogehogheohgoehogheohgoe"
 for i in range(10):
@@ -5,3 +7,6 @@ for i in range(10):
 
 for i in range(20):
     print "hekeheke"
+    print "gaou"
+    print u"何もいうことねえ"
+    print u"何も思いつかねえ"
