53c6b7023a20a31064ef24c6a02f2f241059d77f
kenji <kenji@kenji-Vostro-3350.(none)>
2012-06-07 16:45:48 +0900

    this is a commit of three-commits to test test commit 1 to test
    test commit 2
    to test
    
    these commits are practices
    
    practice for git command
    practice 1
    
    this is the test
    
    practice 2
    
    this is the test2
    
    practice 3
    
    this is the test3

--- test_by_miyama.cpp
+++ test_by_miyama.cpp
@@ -3,4 +3,6 @@
 int main(){
   std::cerr <<"test by miyama" <<std::endl;
   std::cerr << "piyopiyo" << std::endl;
+  std::cerr << "2012-06-07" << std::endl;
+	std::cerr << "hogehoge" << std::endl;
 }
